2343975f2a45066ac3170262c8f0a1f9f7a9d22b
Faraz Chand <faraz.chand@hotmail.com>
2024-03-26 23:10:41 -0400

    general update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./App.scss";
+import { RootPage } from "./pages";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootPage />,
+      children: [{}],
+    },
+  ]);
+
   return <div>App</div>;
 };
 
